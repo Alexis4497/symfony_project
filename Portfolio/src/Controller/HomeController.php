4c87ee0788313e89ecc4dd192986d2927902a5ae
Alexis Caillé <alexiscaille97@gmail.com>
2021-07-01 17:52:56 +0200

    page_about

--- Portfolio/src/Controller/HomeController.php
+++ Portfolio/src/Controller/HomeController.php
@@ -22,4 +22,27 @@ class HomeController
         return new Response($this->twig->render('pages/home.html.twig'));
     }
 
+    public function about(): Response{
+
+        return new Response($this->twig->render('pages/about.html.twig'));
+    }
+
+    public function skills(): Response{
+
+        return new Response($this->twig->render('pages/skills.html.twig'));
+    }
+
+    public function projects(): Response{
+
+        return new Response($this->twig->render('pages/projects.html.twig'));
+    }
+    public function cv(): Response{
+
+        return new Response($this->twig->render('pages/cv.html.twig'));
+    }
+    public function contact(): Response{
+
+        return new Response($this->twig->render('pages/contact.html.twig'));
+    }
+
 }
